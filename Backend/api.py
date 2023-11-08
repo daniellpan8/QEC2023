bd9ceb2edb17c7cdb62c48eada63f23709599015
Nikola Zupancic <nikola.z37@hotmail.com>
2023-11-07 22:26:38 -0500

    added a number to increment in sqlite database

--- Backend/api.py
+++ Backend/api.py
@@ -1,33 +1,82 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import openai
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 CORS(app)
+db = SQLAlchemy(app)
 
 openai.api_key = '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'
+
 
 @app.route('/ask-gpt', methods=['POST'])
 def ask_gpt_chat():
     data = request.json
     messages = data.get("messages")
-    
+
     if not messages:
         return jsonify({"error": "No messages provided."}), 400
-    
+
     try:
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=messages
         )
-        
-       
+
         message = response.choices[0].message["content"].strip()
-        
+
         return jsonify({"message": message})
-    
+
     except openai.error.OpenAIError as e:
         return jsonify({"error": str(e)}), 500
+
+
+# these should not be gets but it was easier for testing so i didnt have to make a button that sends a POST
+
+
+@app.route('/inc', methods=['POST'])
+def inc():
+    x = Data.query.all()
+    x[0].value += 1
+    db.session.commit()
+    return jsonify({"value": x[0].value})
+
+
+@app.route('/dec', methods=['POST'])
+def dec():
+    x = Data.query.all()
+    x[0].value -= 1
+    db.session.commit()
+    return jsonify({"value": x[0].value})
+
+
+@app.route('/getnum', methods=['POST'])
+def getnum():
+    data = Data.query.all()
+    return jsonify({"value": data[0].value})
+
+
+@app.route('/addnum', methods=['POST'])
+def addnum():
+
+    return
+
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Value {self.value}>'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Frontend/website/src/pages/Home.js
+++ Frontend/website/src/pages/Home.js
@@ -1,27 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+const apiBaseUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
 
 const Home = () => {
+
+    const [number, getting] = useState(0);
+
+    const incNum = async () => {
+        try {
+            const response = await fetch(`${apiBaseUrl}/inc`, {
+                method: 'POST',
+            });
+            const result = await response.json();
+            getting(result.value);
+        } catch (error) {
+            console.log('Fetch error:', error);
+        } finally {
+        }
+    }
+
+    const decNum = async () => {
+        try {
+            const response = await fetch(`${apiBaseUrl}/dec`, {
+                method: 'POST',
+            });
+            const result = await response.json();
+            getting(result.value);
+        } catch (error) {
+            console.log('Fetch error:', error);
+        } finally {
+        }
+    }
+
+    const getNum = async () => {
+        try {
+            const response = await fetch(`${apiBaseUrl}/getnum`, {
+                method: 'POST',
+            });
+            const result = await response.json();
+            getting(result.value);
+        } catch (error) {
+            console.log('Fetch error:', error);
+        } finally {
+        }
+    }
+
+    useEffect(() => {
+        getNum();
+    }, []);
+
     return (
         <div className='content-page'>
-            <h1>Home</h1>
-            <p className='c-para'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p className='c-para'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p className='c-para'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p className='c-para'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p className='c-para'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p className='c-para'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
+        <h1>Home</h1>
+        <button className='button' onClick={incNum}>Increment</button>
+        <button className='button' onClick={decNum}>Decrement</button>
+        <h1>{number}</h1>
+        <p className='c-para'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p className='c-para'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p className='c-para'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p className='c-para'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p className='c-para'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p className='c-para'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
         </div>
     );
 };

ac1e9c9634e889bb0a6e5657ea397b635d4f2784
Avedis Boudakian <avedisb30@yahoo.com>
2023-11-17 09:26:38 -0500

    new home page with bouncing animation title/luke neeeeds to push to the repo

--- Frontend/website/src/pages/Home.js
+++ Frontend/website/src/pages/Home.js
@@ -2,81 +2,48 @@ import React, { useState, useEffect } from 'react';
 
 const apiBaseUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
-
 const Home = () => {
+    const [number, setNumber] = useState(0);
 
-    const [number, getting] = useState(0);
-
-    const incNum = async () => {
+    const fetchData = async (action) => {
         try {
-            const response = await fetch(`${apiBaseUrl}/inc`, {
+            const response = await fetch(`${apiBaseUrl}/${action}`, {
                 method: 'POST',
             });
             const result = await response.json();
-            getting(result.value);
+            setNumber(result.value);
         } catch (error) {
             console.log('Fetch error:', error);
-        } finally {
-        }
-    }
-
-    const decNum = async () => {
-        try {
-            const response = await fetch(`${apiBaseUrl}/dec`, {
-                method: 'POST',
-            });
-            const result = await response.json();
-            getting(result.value);
-        } catch (error) {
-            console.log('Fetch error:', error);
-        } finally {
-        }
-    }
-
-    const getNum = async () => {
-        try {
-            const response = await fetch(`${apiBaseUrl}/getnum`, {
-                method: 'POST',
-            });
-            const result = await response.json();
-            getting(result.value);
-        } catch (error) {
-            console.log('Fetch error:', error);
-        } finally {
         }
     }
 
     useEffect(() => {
-        getNum();
+        fetchData('getnum');
     }, []);
 
     return (
-        <div className='content-page'>
-        <h1>Home</h1>
-        <button className='button' onClick={incNum}>Increment</button>
-        <button className='button' onClick={decNum}>Decrement</button>
-        <h1>{number}</h1>
-        <p className='c-para'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p className='c-para'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p className='c-para'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p className='c-para'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p className='c-para'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <p className='c-para'>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+        <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center">
+            <h1 className="text-4xl font-bold mb-4 animate-bounce">QEC WEBSITE!</h1>
+            <div className="flex space-x-4 mb-4">
+                <button 
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => fetchData('inc')}
+                >
+                    Increment
+                </button>
+                <button 
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={() => fetchData('dec')}
+                >
+                    Decrement
+                </button>
+            </div>
+            <h2 className="text-2xl mb-4">{number}</h2>
+            <div className="px-4 py-2 text-center">
+                {/* Your paragraphs with class 'c-para' here */}
+            </div>
         </div>
     );
 };
 
 export default Home;
-

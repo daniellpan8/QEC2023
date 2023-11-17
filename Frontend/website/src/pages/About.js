8e31fc4b4b4dc94640e75fb0b6a3952ae06da89e
lukehunker <lhunker2003@gmail.com>
2023-11-16 20:05:54 -0500

    luke's first commit

--- Frontend/website/src/pages/About.js
+++ Frontend/website/src/pages/About.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+
 function About() {
     return (
         <div className='content-page'>
-            <h1> About </h1>
-            <p className='c-para'> somet text here </p>
+            <h1 class=' mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'> About Us - <span class="text-blue-500">QEC Group #</span> </h1>
+            <p className='c-para'> 
+                <span class = "font-bold underline lg:text-2xl">Team Members: </span><br /> 
+                Luke Hunker<br />  
+                Avedis Boudakian <br /> 
+                Daniell Pan <br /> 
+                Nikola Zupancic
+            </p>
+
+            
         </div>
     );
 }
